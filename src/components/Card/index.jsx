cc7023993d3c9e03587af991322b5cd6f6c2b182
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-19 17:10:53 -0500

    Add product detail functionality to Card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,30 +1,38 @@
 import { useShoppingCartProvider } from "../../Context";
 import { reduceProductName } from "../../utils";
+import { IoMdAdd } from "react-icons/io";
 
-const Card = ({ title, price, image, category }) => {
-  const { addToCart } = useShoppingCartProvider();
-
+const Card = ({ productData }) => {
+  const { addToCart, showProductDetail } = useShoppingCartProvider();
   return (
-    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg drop-shadow-md">
+    <div
+      className="bg-white cursor-pointer w-56 h-60 rounded-lg drop-shadow-md"
+      onClick={() => showProductDetail(productData)}
+    >
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-          {category}
+          {productData.category}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src={image}
+          src={productData.image}
           alt="headphones"
         />
-        <div
-          onClick={addToCart}
-          className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p"
-        >
-          +
-        </div>
+        <button>
+          <IoMdAdd
+            onClick={(e) => {
+              e.stopPropagation();
+              addToCart();
+            }}
+            className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
+          />
+        </button>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">{reduceProductName(title)}</span>
-        <span className="text-sm font-medium">{price}</span>
+        <span className="text-sm font-light">
+          {reduceProductName(productData.title)}
+        </span>
+        <span className="text-sm font-medium">${productData.price}</span>
       </p>
     </div>
   );
