d37cb66b133c8b3a932a9dc3cf4208299dd16f3e
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-18 23:42:25 -0500

    Add Card component

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,27 @@
+import { reduceProductName } from "../../utils";
+
+const Card = ({ title, price, image, category }) => {
+  return (
+    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg drop-shadow-md">
+      <figure className="relative mb-2 w-full h-4/5">
+        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
+          {category}
+        </span>
+        <img
+          className="w-full h-full object-cover rounded-lg"
+          src={image}
+          alt="headphones"
+        />
+        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p">
+          +
+        </div>
+      </figure>
+      <p className="flex justify-between">
+        <span className="text-sm font-light">{reduceProductName(title)}</span>
+        <span className="text-sm font-medium">{price}</span>
+      </p>
+    </div>
+  );
+};
+
+export default Card;
