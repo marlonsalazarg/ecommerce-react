98ae7296a2a90b8dabe5fd7a8618c973abadbca8
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-18 23:42:45 -0500

    Add product listing functionality to Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import Card from "../../components/Card";
 import Layout from "../../components/Layout";
+import { getProducts } from "../../services/product-service";
 
 function Home() {
-  return <Layout className="bg-red-100">Home</Layout>;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getProducts().then((data) => {
+      setItems(data);
+    });
+  }, []);
+
+  return (
+    <Layout className="bg-red-100">
+      Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items.map((item) => {
+          return <Card key={item.id} {...item} />;
+        })}
+      </div>
+    </Layout>
+  );
 }
 
 export default Home;
