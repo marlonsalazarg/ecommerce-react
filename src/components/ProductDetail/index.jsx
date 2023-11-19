07e38a3aea8afa5160a3692d6c1aa09f83c8b187
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-19 17:11:13 -0500

    Refactor ProductDetail component to display product details

--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -2,19 +2,34 @@ import { IoCloseCircle } from "react-icons/io5";
 import { useShoppingCartProvider } from "../../Context";
 
 const ProductDetail = () => {
-  const { isProductDetailOpen, closeProductDetail } = useShoppingCartProvider();
+  const { isProductDetailOpen, closeProductDetail, productToShow } =
+    useShoppingCartProvider();
   return (
     <aside
       className={`${
         isProductDetailOpen ? "flex" : "hidden"
       } flex flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]`}
     >
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center p-4">
         <h2 className="font-medium text-xl">Detail</h2>
         <button>
           <IoCloseCircle className="w-6 h-6" onClick={closeProductDetail} />
         </button>
       </div>
+      <figure className="px-6">
+        <img
+          className="w-full h-full rounded-lg"
+          src={productToShow.image}
+          alt="product"
+        />
+      </figure>
+      <p className="flex flex-col p-6">
+        <span className="font-medium text-2xl mb-2">
+          ${productToShow.price}
+        </span>
+        <span className="font-medium text-md">{productToShow.title}</span>
+        <span className="font-light text-sm">{productToShow.description}</span>
+      </p>
     </aside>
   );
 };
