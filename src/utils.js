1848303e7809026cafd783948f825792e9662d2e
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-20 18:35:06 -0500

    Add totalPrice function

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,7 @@ export function reduceProductName(title){
   const lengthMax = 25;
   return title.length > lengthMax ? title.substring(0, lengthMax) + "..." : title;
 }
+
+export const totalPrice = (products) => {
+  return Math.round(products.reduce((acc, product) => acc + product.price, 0) * 100) / 100;
+}
