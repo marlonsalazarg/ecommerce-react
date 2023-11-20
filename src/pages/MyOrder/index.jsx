33b53d50eac84fae2dfd659e294982a9c5fe49bc
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-20 18:38:06 -0500

    Add route for last order in MyOrders page

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,7 +1,30 @@
+import { useShoppingCartProvider } from "../../Context";
 import Layout from "../../components/Layout";
+import OrderCard from "../../components/OrderCard";
 
 function MyOrder() {
-  return <Layout className="bg-red-100">MyOrder</Layout>;
+  const { order } = useShoppingCartProvider();
+  return (
+    <Layout>
+      <div className="px-3">
+        {order.length > 0 ? (
+          order
+            .slice(-1)[0]
+            .products.map((product) => (
+              <OrderCard
+                key={product.id}
+                id={product.id}
+                title={product.title}
+                imageUrl={product.image}
+                price={product.price}
+              />
+            ))
+        ) : (
+          <p className="text-center">No order yet</p>
+        )}
+      </div>
+    </Layout>
+  );
 }
 
 export default MyOrder;
