e579c445f17bb8481f6630f4a980f76688cc3f03
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-21 10:36:03 -0500

    Add back button to MyOrder page

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,27 +1,32 @@
+import { Link } from "react-router-dom";
 import { useShoppingCartProvider } from "../../Context";
 import Layout from "../../components/Layout";
 import OrderCard from "../../components/OrderCard";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 function MyOrder() {
   const { order } = useShoppingCartProvider();
+  const currentPath = window.location.pathname;
+  let index = currentPath.substring(currentPath.lastIndexOf("/") + 1);
+  if (index === "last") index = order?.length - 1;
   return (
     <Layout>
+      <div className="flex items-center justify-center relative w-80 mb-6">
+        <Link to="/my-orders" className="absolute left-0">
+          <IoMdArrowRoundBack className="h-6 w-6 cursor-pointer" />
+        </Link>
+        <h1>My Order</h1>
+      </div>
       <div className="px-3">
-        {order.length > 0 ? (
-          order
-            .slice(-1)[0]
-            .products.map((product) => (
-              <OrderCard
-                key={product.id}
-                id={product.id}
-                title={product.title}
-                imageUrl={product.image}
-                price={product.price}
-              />
-            ))
-        ) : (
-          <p className="text-center">No order yet</p>
-        )}
+        {order?.[index]?.products.map((product) => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            imageUrl={product.image}
+            price={product.price}
+          />
+        ))}
       </div>
     </Layout>
   );
