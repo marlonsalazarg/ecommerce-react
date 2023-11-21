7bd5f9ce3dd70d90faea5ea4be50497234e03398
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-21 10:36:38 -0500

    Add dynamic rendering of orders in MyOrders page

--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+import { useShoppingCartProvider } from "../../Context";
 import Layout from "../../components/Layout";
+import OrdersCard from "../../components/OrdersCard";
 
 function MyOrders() {
-  return <Layout className="bg-red-100">MyOrders</Layout>;
+  const { order } = useShoppingCartProvider();
+
+  return (
+    <Layout>
+      <div className="flex items-center justify-center relative w-80 mb-6">
+        <h1 className="font-medium text-xl">My Orders</h1>
+      </div>
+      {order.length > 0 ? (
+        order?.map((order, index) => {
+          return (
+            <Link key={index} to={`/my-orders/${index}`}>
+              <OrdersCard
+                totalPrice={order.totalPrice}
+                totalProducts={order.totalItems}
+              />
+            </Link>
+          );
+        })
+      ) : (
+        <p className="text-center">No order yet...</p>
+      )}
+    </Layout>
+  );
 }
 
 export default MyOrders;
