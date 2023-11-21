4ad9bb6f0f05a59dd07326ac934bb5f824fa0d54
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-21 11:42:34 -0500

    Refactor Home component and add search functionality

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,24 @@
-import { useState, useEffect } from "react";
+import { useShoppingCartProvider } from "../../Context";
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
-import { getProducts } from "../../services/product-service";
 import ProductDetail from "../../components/ProductDetail";
 
 function Home() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    getProducts().then((data) => {
-      setItems(data);
-    });
-  }, []);
+  const { items, setSearchByTitle } = useShoppingCartProvider();
 
   return (
-    <Layout className="bg-red-100">
-      Home
+    <Layout>
+      <div className="flex items-center justify-center relative w-80 mb-6">
+        <h1 className="font-medium text-xl">Exclusive Products</h1>
+      </div>
+      <input
+        type="text"
+        placeholder="Search a product"
+        className="border border-black rounded-lg w-80 p-4 mb-4 focus:outline-none"
+        onChange={(e) => {
+          setSearchByTitle(e.target.value);
+        }}
+      />
       <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
         {items.map((item) => {
           return <Card key={item.id} productData={item} />;
