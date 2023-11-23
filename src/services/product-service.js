315807a4970f180e9f87b551d748a70aabea8011
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-22 19:41:16 -0500

    Refactor category names in product-service.js

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,7 +1,8 @@
+import { editCategoriesName } from "../utils";
 const BASE_URI="https://fakestoreapi.com";
 
 export async function getProducts() {
     const response = await fetch(`${BASE_URI}/products`);
     const products = await response.json();
-    return products;
+    return editCategoriesName(products);
 }
--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,11 @@ export function reduceProductName(title){
 export const totalPrice = (products) => {
   return Math.round(products.reduce((acc, product) => acc + product.price, 0) * 100) / 100;
 }
+
+export const editCategoriesName = (products) => products.map((item) => {
+  let category = item.category;
+  category = category.replace(/'/g, "");
+  category = category.replace(/ /g, "-");
+  item.category = category;
+  return item;
+} );
